4863fb22b36c621c0b83872f87f19c234dbd42e2
jonyeeha <jonyeeha@yahoo.com>
2026-03-26 14:06:08 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import CompletePage from "./complete.jsx";
+
+// Simple router: /complete?token=xxx goes to CompletePage, everything else to App
+const isComplete = window.location.pathname === "/complete";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {isComplete ? <CompletePage /> : <App />}
+  </React.StrictMode>
+);
