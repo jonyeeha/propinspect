c9527a24167a8ecbc77587115bac182527943c32
jonyeeha <jonyeeha@yahoo.com>
2026-03-26 14:06:46 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // Ensure /complete route is handled by the SPA
+  server: { historyApiFallback: true },
+});
