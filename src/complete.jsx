7272d7afe7e8759474d0f299f0b6591e3cda3912
jonyeeha <jonyeeha@yahoo.com>
2026-03-27 19:40:08 -0700

    Update complete.jsx

--- src/complete.jsx
+++ src/complete.jsx
@@ -1,9 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const SUPA_URL = "https://efhbnddgcazzkbppzdqw.supabase.co";
-const SUPA_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmaGJuZGRnY2F6emticHB6ZHF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1NTAzMjYsImV4cCI6MjA5MDEyNjMyNn0.BmEoq6jAq_2U0fK13YnCB9rtmblI5Cse3P-9-qtOkfA";
-const supabase = createClient(SUPA_URL, SUPA_KEY);
+import { supabase } from "./supabaseClient.js";
 
 const toDataURL = f => new Promise(res => { const r = new FileReader(); r.onload = e => res(e.target.result); r.readAsDataURL(f); });
 
